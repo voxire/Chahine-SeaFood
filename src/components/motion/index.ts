1e6ad927e450b9732fd1a95fa8126e9a7296b862
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-04-22 12:15:53 +0300

    Add motion barrel export

---
+++ src/components/motion/index.ts
@@ -0,0 +1,5 @@
+export { FadeIn } from "./FadeIn";
+export { Parallax } from "./Parallax";
+export { ScrollProgress } from "./ScrollProgress";
+export { Pill } from "./Pill";
+export { SectionHeading } from "./SectionHeading";
