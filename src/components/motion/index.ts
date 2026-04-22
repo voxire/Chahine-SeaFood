98ffd387c0663863b63a5ffb72cff0a99c37badc
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-04-22 11:38:50 +0000

    Export MagneticButton and SplitText from motion barrel

--- src/components/motion/index.ts
+++ src/components/motion/index.ts
@@ -3,3 +3,5 @@ export { Parallax } from "./Parallax";
 export { ScrollProgress } from "./ScrollProgress";
 export { Pill } from "./Pill";
 export { SectionHeading } from "./SectionHeading";
+export { MagneticButton } from "./MagneticButton";
+export { SplitText } from "./SplitText";
